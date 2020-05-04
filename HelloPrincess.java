0446734973bbcd929134e264aad4b248b5f591a5
khushboo <khushboo.rani@gmail.com>
2020-05-04 17:35:33 +0530

    making changes in first branch

---
+++ HelloPrincess.java
@@ -0,0 +1,15 @@
+
+public class HelloPrincess{
+
+	public static void main(String[] args){
+	
+		for(int i=0;i<=10;i++)
+			{
+				System.out.println("Hello Princess" +" "+ i);
+			
+			}
+	}
+}
+
+//javac Hello.java
+//java H
